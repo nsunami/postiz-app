e8049716d17f39778a3706675a86a510131f0d76
Nami Sunami <nsunami@pm.me>
2026-08-06 21:13:19 +0200

    fix(bluesky): allow atproto hosted at subdomain

--- libraries/nestjs-libraries/src/integrations/social/bluesky.provider.ts
+++ libraries/nestjs-libraries/src/integrations/social/bluesky.provider.ts
@@ -261,7 +261,7 @@ export class BlueskyProvider extends SocialAbstract implements SocialProvider {
         key: 'service',
         label: 'Service',
         defaultValue: 'https://bsky.social',
-        validation: `/^(https?:\\/\\/)?((([a-zA-Z0-9\\-_]{1,256}\\.[a-zA-Z]{2,6})|(([0-9]{1,3}\\.){3}[0-9]{1,3}))(:[0-9]{1,5})?)(\\/[^\\s]*)?$/`,
+        validation: `/^(https?:\/\/)?((([a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,})|((?:25[0-5]|2[0-4]\d|1?\d?\d)(?:\.(?:25[0-5]|2[0-4]\d|1?\d?\d)){3}))(?::\d{1,5})?(\/[^\s?#]*)?(\?[^\s#]*)?(#\S*)?$/`,
         type: 'text' as const,
       },
       {
